9ac256d695c0c7b9066ddd66c4670adc56e451fd
agerashchenko-pixel <a.gerashchenko@g.nsu.ru>
2026-04-17 23:08:39 +0700

    Update 9.2.py

--- 9.2.py
+++ 9.2.py
@@ -1,5 +1,5 @@
-with open('input', 'r', encoding='utf-8') as f_in, \
-        open('output', 'w', encoding='utf-8') as f_out:
+with (open('input', 'r', encoding='utf-8') as f_in,
+      open('output', 'w', encoding='utf-8') as f_out):
     for line in f_in:
         if line.startswith(('A', 'a')):
             f_out.write(line)

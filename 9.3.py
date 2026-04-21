ec3e1c47993a24fbd9acc27d482db1099aea788f
agerashchenko-pixel <a.gerashchenko@g.nsu.ru>
2026-04-21 19:05:29 +0700

    Update 9.3.py

--- 9.3.py
+++ 9.3.py
@@ -4,6 +4,7 @@ with open('input', 'r', encoding='utf-8') as f_in:
         if line.strip():
             first_char = line[0]
             symbols.append(first_char)
+
     result = "".join(symbols)
 
 with open('output', 'w', encoding='utf-8') as f_out:

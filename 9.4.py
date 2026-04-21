fade47ab9f2fe41a644e8b9d2bb05220bc1b3165
agerashchenko-pixel <a.gerashchenko@g.nsu.ru>
2026-04-21 19:06:07 +0700

    Update 9.4.py

--- 9.4.py
+++ 9.4.py
@@ -1,5 +1,7 @@
-with (open('input', 'r', encoding='utf-8') as f_in, 
-        open('output', 'w', encoding='utf-8') as f_out):
+with (
+    open('input', 'r', encoding='utf-8') as f_in,
+    open('output', 'w', encoding='utf-8') as f_out
+):
     for line in f_in:
         if len(line.rstrip()) > 20:
             f_out.write(line)

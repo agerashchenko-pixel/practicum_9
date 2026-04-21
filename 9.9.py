290dc788e3a29864bad2b1d1e9bbfd37f8201915
agerashchenko-pixel <a.gerashchenko@g.nsu.ru>
2026-04-21 19:08:28 +0700

    Update 9.9.py

--- 9.9.py
+++ 9.9.py
@@ -1,11 +1,13 @@
 import os
 
+
 os.makedirs('simple', exist_ok=True)
 output_path = os.path.join('simple', 'output')
-with (open('input', 'r', encoding='utf-8') as f_in, 
-        open(output_path, 'w', encoding='utf-8') as f_out):
+
+with (
+    open('input', 'r', encoding='utf-8') as f_in,
+    open(output_path, 'w', encoding='utf-8') as f_out
+):
     for i, line in enumerate(f_in, start=1):
         if i % 2 == 0:
             f_out.write(line)
-
-

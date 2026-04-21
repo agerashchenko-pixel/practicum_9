611e04e003e74823dad700c62b84b846b3e82f13
agerashchenko-pixel <a.gerashchenko@g.nsu.ru>
2026-04-21 19:08:01 +0700

    Update 9.7.py

--- 9.7.py
+++ 9.7.py
@@ -1,5 +1,7 @@
-with (open('input', 'r', encoding='utf-8') as f_in, 
-        open('output', 'w', encoding='utf-8') as f_out):
+with (
+    open('input', 'r', encoding='utf-8') as f_in,
+    open('output', 'w', encoding='utf-8') as f_out
+):
     for line in f_in:
         if line.strip() != '100':
             f_out.write(line)
